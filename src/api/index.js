9d12501b85366d0e5ebc808de9252df98a432304
Ramón París <pariis78@gmail.com>
2020-01-27 23:36:55 -0300

    Swagger integration

--- src/api/index.js
+++ src/api/index.js
@@ -3,12 +3,15 @@ require("dotenv").config();
 const express = require("express");
 const config = require("../config");
 const user = require("./components/user/userNetwork");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDoc = require("./swagger.json");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/user", user);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(config.api.port, () => {
   console.log(`API listening on port ${config.api.port} 🚀`);
